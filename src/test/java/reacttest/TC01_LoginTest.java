4850574fea28300337bed3837d068c2339aeeba3
Jaffar Shaik <jaffars@tfg.co.za>
2021-03-21 17:20:07 +0200

    first commit

---
+++ src/test/java/reacttest/TC01_LoginTest.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package reacttest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @author jaffar
+ *
+ */
+public class TC01_LoginTest {
+	
+	ChromeDriver driver;
+	
+	
+	@Test 
+	
+	public void loginTest() throws InterruptedException {
+		
+     
+         WebDriverManager.chromedriver().setup();
+		  
+         WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("https://www.saucedemo.com/");
+			 
+         Thread.sleep(4000);
+		 
+		 driver.manage().window().maximize();
+		 
+		 driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		 
+		 driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		 
+		 driver.findElement(By.xpath("//input[@class='btn_action']")).click();
+		 
+		 Thread.sleep(4000);
+		 
+	     String curl = driver.getCurrentUrl();
+	   
+	     System.out.println("My application home page url :" + curl);
+		 
+	     Thread.sleep(2000);
+		 
+		 driver.quit();
+		
+	}
+}
+	   
+		
+
